15b44b735dfb954ca6cb240001b96585175b7c34
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2025-05-23 19:59:59 +0900

    タスク一覧機能実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,18 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
+  #before_action :set_todo, only: [:show, :edit, :update, :destroy, :toggle_complete]
+
   def index
+    @todos = current_user.todos
+    @todos = @todos.order(created_at: :desc)# 新しく作ったものから順番に表示したいので並び替え
+
+  end
+
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :completed)
   end
 end
